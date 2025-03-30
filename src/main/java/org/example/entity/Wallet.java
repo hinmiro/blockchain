2de46d98b662b8a888aec0b8b48f839cda3da4d2
mirohi <miro.hintikka@metropolia.fi>
2025-03-30 22:30:28 +0300

    Add Wallet entity with key pair generation

---
+++ src/main/java/org/example/entity/Wallet.java
@@ -0,0 +1,43 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+@Entity
+@Setter
+@Getter
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public Wallet() { generateKeyPair(); }
+
+    private void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException("Key generation exception: " +e.getMessage());
+        }
+    }
+}
