88c9804281f2eb5660dcb9fdd2a96afc5a9beca9
mirohi <miro.hintikka@metropolia.fi>
2025-03-30 23:26:57 +0300

    Implement ECDSA signature generation and verification in Transaction class; add TransactionInput and TransactionOutput classes

--- src/main/java/org/example/entity/Transaction.java
+++ src/main/java/org/example/entity/Transaction.java
@@ -5,6 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.example.util.StringUtil;
+
+import java.math.BigInteger;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "transactions")
@@ -17,20 +23,49 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String sender;
-    private String receiver;
-    private double amount;
+    private String transactionId;
+    private PublicKey sender;
+    private PublicKey recipient;
+    private double value;
     private long timestamp;
+    private byte[] signature;
+
+    private ArrayList<TransactionInput> inputs = new ArrayList<>();
+    private ArrayList<TransactionOutput> outputs = new ArrayList<>();
+
+    private static BigInteger sequence = BigInteger.ZERO;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name= "block_hash")
+    @JoinColumn(name = "block_hash")
     private Block block;
 
 
-    public Transaction(String sender, String receiver, double amount) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.amount = amount;
+    public Transaction(PublicKey from, PublicKey to, double value, ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.recipient = to;
+        this.value = value;
+        this.inputs = inputs;
         this.timestamp = System.currentTimeMillis();
+    }
+
+    public String calculateHash() {
+        sequence = sequence.add(BigInteger.ONE);
+
+        return StringUtil.apply(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(recipient) +
+                        Double.toString(value) + sequence.toString()
+
+        );
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Double.toString(value);
+        signature = StringUtil.applyECDSASig(privateKey, data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Double.toString(value);
+        return StringUtil.verifyECDSASig(sender, data, signature);
     }
 }
--- src/main/java/org/example/util/StringUtil.java
+++ src/main/java/org/example/util/StringUtil.java
@@ -1,7 +1,8 @@
 package org.example.util;
 
 
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
 
 public class StringUtil {
 
@@ -19,5 +20,38 @@ public class StringUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //Applies ECDSA Signature and returns the result ( as bytes )
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSign = dsa.sign();
+            output = realSign;
+        } catch (Exception e) {
+            throw new RuntimeException("Apply ECDSA exception: " + e.getMessage());
+        }
+        return output;
+    }
+
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        } catch (Exception e) {
+            throw new RuntimeException("Error occurred in ECDSA verification: " + e.getMessage());
+        }
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
